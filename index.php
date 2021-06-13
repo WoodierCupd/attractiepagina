268304a0c762373c4efbc59107da2b8f55ec54a7
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2021-06-13 22:01:46 +0200

    test

--- index.php
+++ index.php
@@ -18,13 +18,29 @@ require_once 'admin/backend/config.php';
 </head>
 
 <body>
-
+    <?php
+    require_once 'backend/conn.php';
+    $query = "SELECT * FROM rides";
+    $statement = $conn->prepare($query);
+    $statement->execute();
+    $rides = $statement->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+    
     <?php require_once 'header.php'; ?>
     <div class="container content">
         <aside>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia modi dolore magnam! Iste libero voluptatum autem, sapiente ullam earum nostrum sed magnam vel laboriosam quibusdam, officia, esse vitae dignissimos nulla?
         </aside>
         <main>
+            <?php foreach($rides as $ride): ?>
+                <tr>
+                    <td><?php echo $ride['title']; ?></td>
+                    <td><?php echo $ride['themeland']; ?></td>
+                    <td><?php echo $ride['min_length']; ?></td>
+                    <td><?php echo $ride['fast_pass']; ?></td>
+                    <td><a href="edit.php?id=<?php echo $ride['id']; ?>">aanpassen</a></td>
+                </tr>
+            <?php endforeach; ?>
             <!-- hier komen de attractiekaartjes -->
         </main>
     </div>
